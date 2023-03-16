d611b8cb138b4a5014d92dbabbd20f771b0807c2
Francisco Amaya <francisco2240000@gmail.com>
2023-03-16 17:10:41 -0600

    practicando la desestructuracion en javascript

--- arrays.js
+++ arrays.js
@@ -1,2 +1,10 @@
-const fruits = ["mango","papaya","manzana","mamon","jocote"];
+const fruits = ["mango", "papaya", "manzana", "mamon", "jocote"];
 fruits.map((fruit) => console.log(`la fruta es = ${fruit}`))
+const animals = ["horse", "cat", "roster", "dog"];
+let [animal1, animal2, animal3, animal4] = animals;
+// empuja el elemento al final  del array
+animals.push("elephant");
+console.log(animal1, animal2, animal3, animal4);
+for (const key in animals) {
+  console.log(animals[key]);
+}
